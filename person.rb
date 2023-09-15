a9bd032419a6601bce0e1f6d3083987297952d55
geekelo <efuelight12@gmail.com>
2023-09-15 15:16:42 +0100

    Correct EOL for all files to LF

---
+++ person.rb
@@ -0,0 +1,27 @@
+class Person
+
+	attr_resders :id, :name, :age
+
+	def initialize(id, age, name = 'unknown', parent_permission = true)
+		@id = id
+		@age = age
+		@name = name
+		@parent_permission = parent_permission
+	end
+
+	def name = (new_name)
+		@name = name
+	end
+
+	def age = (new_age)
+		@age = age
+	end
+
+	def can_use_services?
+		of_age? || @parent_permission
+
+	private
+
+	def of_age?
+		@age >= 18
+	end
