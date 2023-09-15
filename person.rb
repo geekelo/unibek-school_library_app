0b3cc19d345936fe0a0394ddbef98fcf1115c8ef
geekelo <efuelight12@gmail.com>
2023-09-15 15:51:59 +0100

    Replace def with attr-writer for setters

--- person.rb
+++ person.rb
@@ -1,18 +1,18 @@
 class Person
   attr_reader :id, :name, :age
 
-  def initialize(id, age, name = 'unknown', parent_permission = true)
+  def initialize(id, age, name = 'unknown', parent_permission: true)
     @id = id
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  def name=(new_name)
+  attr_writer name=(new_name)
     @name = new_name
   end
 
-  def age=(new_age)
+  attr_writer age=(new_age)
     @age = new_age
   end
 
