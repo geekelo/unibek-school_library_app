55aca448bc8606798f00b8b0b1f87ffe72851b95
geekelo <efuelight12@gmail.com>
2023-09-15 15:49:39 +0100

    Fix the linter errows in person file

--- person.rb
+++ person.rb
@@ -2,27 +2,27 @@ class Person
   attr_reader :id, :name, :age
 
   def initialize(id, age, name = 'unknown', parent_permission = true)
-  	@id = id
-  	@age = age
-  	@name = name
-  	@parent_permission = parent_permission
+    @id = id
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
   end
 
   def name=(new_name)
-  	@name = new_name
+    @name = new_name
   end
 
   def age=(new_age)
-  	@age = new_age
+    @age = new_age
   end
 
   def can_use_services?
-  	of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   private
 
   def of_age?
-  	@age >= 18
+    @age >= 18
   end
 end
