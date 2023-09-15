871c70a884e1718d3cb9f3165f8243e4a11c6d13
geekelo <efuelight12@gmail.com>
2023-09-15 15:47:58 +0100

    Fix the linter errows in person file

--- person.rb
+++ person.rb
@@ -1,28 +1,28 @@
 class Person
-	attr_reader :id, :name, :age
+  attr_reader :id, :name, :age
 
-	def initialize(id, age, name = 'unknown', parent_permission = true)
-		@id = id
-		@age = age
-		@name = name
-		@parent_permission = parent_permission
-	end
+  def initialize(id, age, name = 'unknown', parent_permission = true)
+  	@id = id
+  	@age = age
+  	@name = name
+  	@parent_permission = parent_permission
+  end
 
-	def name=(new_name)
-		@name = name
-	end
+  def name=(new_name)
+  	@name = new_name
+  end
 
-	def age=(new_age)
-		@age = age
-	end
+  def age=(new_age)
+  	@age = new_age
+  end
 
-	def can_use_services?
-		of_age? || @parent_permission
-	end
+  def can_use_services?
+  	of_age? || @parent_permission
+  end
 
-	private
+  private
 
-	def of_age?
-		@age >= 18
-	end
+  def of_age?
+  	@age >= 18
+  end
 end
