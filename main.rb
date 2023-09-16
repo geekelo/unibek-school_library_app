de5611bb07d011d3475704e7f85c3bb27cbeba6a
geekelo <efuelight12@gmail.com>
2023-09-16 20:37:18 +0100

    Fix linters

--- main.rb
+++ main.rb
@@ -5,14 +5,14 @@ def main
   people = [] # Initialize an array to store people
 
   loop do
-    puts "Choose an option:"
-    puts "1. List books"
-    puts "2. List people"
-    puts "3. Create person"
-    puts "4. Create book"
-    puts "5. Create rental"
-    puts "6. List rentals for a person"
-    puts "7. Quit"
+    puts 'Choose an option:'
+    puts '1. List books'
+    puts '2. List people'
+    puts '3. Create person'
+    puts '4. Create book'
+    puts '5. Create rental'
+    puts '6. List rentals for a person'
+    puts '7. Quit'
 
     choice = gets.chomp.to_i
 
@@ -25,7 +25,7 @@ def main
     when 7
       break
     else
-      puts "Invalid choice. Please try again."
+      puts 'Invalid choice. Please try again.'
     end
   end
 end
