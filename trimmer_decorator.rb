453cd4677b475e4dc2978c00b9c3e3ccbe6a91a2
geekelo <efuelight12@gmail.com>
2023-09-16 19:24:57 +0100

    Fix linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,6 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name[0, 10] # Trim to a maximum of 10 characters
+    super[0, 10] # Trim to a maximum of 10 characters
   end
 end
