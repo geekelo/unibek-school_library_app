f4b9ea7469444b94d1d0bb3a4c8316df74d0aad2
geekelo <efuelight12@gmail.com>
2023-09-16 23:09:41 +0100

    Fix linter errors

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'book'
 require_relative 'person'
 require_relative 'student'
--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Nameable
   def correct_name
     raise NotImplementedError, 'Sub methods must implement this method'
